c2849a7acd7eef4e3f7f2854cf3c141935f1e888
Rubem gonzaga <rnanclarez@gmail.com>
2020-09-11 21:18:54 -0300

    Acho que ficou mais complicado do que antes kkk

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
-function pegaECalcula() {
-    var va = document.getElementById("myVA").value;
-    var ca = document.getElementById("myCA").value;
-    var vb = document.getElementById("myVB").value;
-    var cb = document.getElementById("myCB").value;
-    
-    var resultadoA = ca/va*100
-    var resultadoB = cb/vb*100
-    
 
-    if (resultadoA > resultadoB) {
-        var difAWin = resultadoA-resultadoB
-        alert("A página A teve mais conversões! Você converteu: "+parseInt(resultadoA)+"% de todos os seus visitantes. Resultando em: "+parseInt(difAWin)+"% de diferença em relação a pagina B, que converteu somente: "+parseInt(resultadoB)+"%")
-    } else if (resultadoB > resultadoA) {
-        var difBWin = resultadoB-resultadoA
-        alert("A página B teve mais conversões! Você converteu: "+parseInt(resultadoB)+"% de todos os seus visitantes. Resultando em: "+parseInt(difBWin)+"% de diferença em relação a pagina A, que converteu somente: "+parseInt(resultadoA)+"%")
-    } else {
-        return alert("Os resultados foram iguais, ambas as paginas converteram: "+parseInt(resultadoB)+"%")
+document.addEventListener("DOMContentLoaded",() => {
+    document.getElementById("calcula")
+    .addEventListener("click",() => {
+        var visitantesA = document.getElementById("myVA").value;
+        var conversoesA = document.getElementById("myCA").value;
+        var visitantesB = document.getElementById("myVB").value;
+        var conversoesB = document.getElementById("myCB").value;
+
+        let A = siteFabric("A", visitantesA, conversoesA);
+        let B = siteFabric("B", visitantesB, conversoesB);
+
+        window.alert(criaMensagem(A,B));
+
+    }); 
+
+    siteFabric = (nome, visitantes, conversoes) => {
+        
+        let resultado = Math.trunc((conversoes / visitantes) * 100);
+        return {
+            nome:nome,
+            resultado: resultado
+        }
     }
-//document.getElementById("result").innerHTML("teste")
 
-};
+
+    criaMensagem = (siteA, siteB) =>{
+        let mensagem = '';
+        
+        if(siteA.resultado === siteB.resultado){
+            mensagem = `Os resultados foram iguais, ambas as paginas converteram: ${siteA.resultado}%` 
+        }else{
+            let maior = max([siteA, siteB])
+            let menor = min([siteB, siteA])
+            let diferenca = Math.abs(siteA.resultado - siteB.resultado);
+
+            mensagem = `A página ${maior.nome}  teve mais conversões! Você converteu: 
+            ${maior.resultado}% de todos os seus visitantes. Resultando em: 
+            ${diferenca}% de diferença em relação a pagina ${menor.nome}, que converteu somente: 
+            ${menor.resultado}%`;
+        }
+        return mensagem;
+    }
+
+    min = arr => arr.reduce( (p, v) =>  p.resultado < v.resultado ? p : v );
+      
+    max = arr => arr.reduce((p, v) => p.resultado > v.resultado ? p : v );
+
+})
